caee5baca519f9192d7eea6a75adf1be01f4cb4d
grzzr <98114095+grzzr@users.noreply.github.com>
2024-11-01 15:22:24 -0300

    assets 1-21

---
+++ images.g.ts
@@ -0,0 +1,424 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "ukdpZYyQ":
+            case "1":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "-Y-^:XMO#Z:NP0xsdQ~+":
+            case "2":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f f f f f f 1 f f 1 f f f f f f 
+f f f f f 1 f 1 1 f 1 f f f f f 
+f f f f f 1 f 1 1 f 1 f f f f f 
+f f f f f f 1 f f 1 f f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "=Z`r4~}IGM*0S#;Z})lp":
+            case "3":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f 1 1 f f 1 1 f f f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f f f f 1 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "~Czh[_(?-8rJ60v`0BO`":
+            case "4":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+`;
+            case "]Ez:wh+fmkd-KX^U}-j]":
+            case "5":return img`
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+`;
+            case "rO5vY%RMJ:y=n[jIF_g}":
+            case "6":return img`
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "vTw(CP:z@Mup%aD+0jV*":
+            case "7":return img`
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f f f f f f f f f f f f f f f 
+f 1 1 1 1 f f f f f f 1 1 1 1 f 
+f 1 1 1 1 f f f f f f 1 1 1 1 f 
+`;
+            case ")?d%^^[PaV+3KJ4BHJW@":
+            case "8":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f 1 1 f f f f f f f f f f f f f 
+f 1 1 f f f f f f f f f f f f f 
+f f f 1 f f f f f f f f f f f f 
+f 1 1 f 1 1 1 f f f f f f f f f 
+f 1 1 f f f f 1 1 1 1 f f f f f 
+f 1 1 f f f f f f f f 1 1 1 1 1 
+f 1 1 f f f f f f f f f f f f f 
+f 1 1 f f f f f f f f f f f f f 
+`;
+            case "/%=Bsocc}q1;+IWE0B;1":
+            case "9":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 f f f f f f f f f f f f f 
+1 1 1 f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 1 f 1 1 1 f f 1 1 1 f f 1 1 
+1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 f 1 f 1 1 1 1 f 1 1 1 1 f 
+1 1 f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case ":I/yY(wPw[OUIaJRy7#k":
+            case "10":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+f f f f f f f f f f f f f f f f 
+1 1 f f f f f f 1 1 1 f f f f f 
+1 1 f 1 1 1 f 1 1 1 f 1 1 1 1 1 
+1 1 f f f f 1 1 1 f f f f f f f 
+1 1 f f f 1 1 1 f f f f f f f f 
+1 1 f f 1 1 1 f f f f f f f f f 
+1 1 f 1 1 1 f f f f f f f f f f 
+1 1 f 1 1 f f f f f f f f f f f 
+1 1 f 1 f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+`;
+            case "[dJBdbRKO7pO`S;ZaQ=,":
+            case "11":return img`
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+`;
+            case "z(aFdAs2Mnj2X`[vfX6l":
+            case "12":return img`
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+1 1 f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+1 1 1 f f f f f f f f f f f f f 
+`;
+            case "jy7!%10eJfkttP!DOJV]":
+            case "13":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+f f 1 1 1 f f f f f f 1 1 1 f f 
+f f 1 f 1 f 1 1 1 1 f 1 f 1 f f 
+f f f 1 f 1 1 1 1 1 1 f 1 f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f 1 1 f 1 1 f f 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+`;
+            case "laLU9bbN^@c)^@Jytg%U":
+            case "14":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+f f f f 1 f 1 1 1 1 f 1 f f f f 
+f f f 1 f 1 1 1 1 1 1 f 1 f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f 1 1 f 1 1 f f 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+`;
+            case ")~zk(Kc-$1WvtfrM%INa":
+            case "15":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 1 1 f f f f f f f f 1 1 f f 
+f f 1 1 1 f f f f f f 1 1 1 f f 
+f f f f 1 f 1 1 1 1 f 1 f f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f 1 1 f 1 1 f f 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+`;
+            case "ktTxca,9fI19kx*@(^zF":
+            case "16":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 1 f f f f f f f f f f 1 f f 
+f f 1 1 1 1 f f f f 1 1 1 1 f f 
+f f f 1 1 f 1 1 1 1 f 1 1 f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f f f 1 1 f 1 1 f 1 1 f f f f 
+f f 1 1 f 1 1 f f 1 1 f 1 1 f f 
+f f 1 1 f 1 1 1 1 1 1 f 1 1 f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+`;
+            case "ywCuZs=Hi}x2K`c0O/,M":
+            case "17":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 f f 1 f f 1 f f 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+`;
+            case "1l,M]]zWk`Yu1jBwY%":
+            case "18":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f 1 f f f f f f 1 f f f f 
+f f f 1 f f f f f f f f 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+f f f 1 1 f 1 1 1 1 f 1 1 f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 f f 1 f f 1 f f 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+`;
+            case "f8eP0YxO3!0-J|+5L*Ta":
+            case "19":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f 1 1 1 1 1 1 f f f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f 1 1 1 1 1 1 1 1 1 1 1 1 f f 
+f f 1 1 1 1 1 1 1 1 1 1 1 1 f f 
+f f 1 f f f f f f f f f f 1 f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f 1 f f f f f f f f f f 1 f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 f f 1 f f 1 f f 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+`;
+            case "*crTJ*Q0j2#~ac{n6XVW":
+            case "20":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f 1 f 1 1 f 1 f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 1 1 1 1 1 1 1 1 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f 1 f f 1 1 1 1 f f 1 f f f 
+f f f f 1 1 1 1 1 1 1 1 f f f f 
+f f f 1 f f 1 f f 1 f f 1 f f f 
+f f f 1 1 f f f f f f 1 1 f f f 
+f f f f 1 f f f f f f 1 f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+            case "[aaV*RZ*u`Qej3PB(#nv":
+            case "21":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f f 1 f f f f 1 f f f f f 
+f f f f f 1 f 1 1 f 1 f f f f f 
+f f f f f 1 f 1 1 f 1 f f f f f 
+f f f f f 1 f f f f 1 f f f f f 
+f f f f f f 1 1 1 1 f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
